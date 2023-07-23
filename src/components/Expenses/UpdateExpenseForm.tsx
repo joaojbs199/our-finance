61612b98cdded259aed9914dddb9773a5dd591d9
João Batista da Silva <joaojbs199@gmail.com>
2023-07-23 06:51:37 -0300

    feat: create update expense form header

--- src/components/Expenses/UpdateExpenseForm.tsx
+++ src/components/Expenses/UpdateExpenseForm.tsx
@@ -18,16 +18,19 @@ export const UpdateExpenseForm = () => {
 
   return (
     <>
-      {uiState.dialogs.updateExpenseDialog.isOpen && (
+      {!uiState.dialogs.updateExpenseDialog.isOpen && (
         <BlockBackground>
-          <div className="z-30 h-3/5 w-[95%] bg-red-500">
-            <X
-              onClick={() => {
-                dispatch(
-                  ExpenseActions.setIsOpenUpdateExpenseDialog({ isOpen: false, expenseId: 0 }),
-                );
-              }}
-            />
+          <div className="z-30 h-3/5 w-[95%] max-w-md rounded-md bg-neutral-800">
+            <header className="flex w-full justify-end border-b border-neutral-700 p-1 sm:p-2">
+              <X
+                className="h-4 w-4 rounded-full border border-neutral-600 p-0.5 text-gray-50 hover:cursor-pointer hover:border-gray-500 hover:text-neutral-400 sm:h-5 sm:w-5"
+                onClick={() => {
+                  dispatch(
+                    ExpenseActions.setIsOpenUpdateExpenseDialog({ isOpen: false, expenseId: 0 }),
+                  );
+                }}
+              />
+            </header>
           </div>
         </BlockBackground>
       )}
